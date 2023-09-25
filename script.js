577f75dc9a456405877bf534c47b5b6a100880fe
Gabriel Guimaraes <gabriel.mg04@outlook.com>
2023-09-25 19:07:52 +0100

    Added buttons Correct styling and selection pattern

--- script.js
+++ script.js
@@ -3,5 +3,19 @@ const colorPicker = document.querySelector('#color-picker');
 
 colorPicker.addEventListener('input', () => {
     colorWrapper.style.cssText = `border-color: ${colorPicker.value}; background-color: ${colorPicker.value};`;
+});
 
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        if (button.textContent === 'Clear') {
+            return;
+        };
+
+        buttons.forEach((btn) => {
+            btn.classList.remove('selected');
+        });
+
+        button.classList.toggle('selected');
+    });
 });
